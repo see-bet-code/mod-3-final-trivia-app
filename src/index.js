c2cfa79604b4d3c36fdb26cc40d62aa3891f707d
see-bet-code <brittani.elan.taylor@gmail.com>
2020-11-09 17:21:35 -0500

    fetch test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+const BASE_URL = 'https://opentdb.com/api.php?amount=10'
 console.log('Hello World')
 // const h1 = document.createElement('h1')
 // h1.innerHTML = 'Hello World Title'
 // document.append(h1)
+
+// in form create game and fetch (dynamic url based on form input)
+fetch(BASE_URL)
+.then(r => r.json())
+.then(console.log)
+
+//
