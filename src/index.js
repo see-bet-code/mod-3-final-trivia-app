cb675834420d300724fba4e16b2ad81b706b9646
see-bet-code <brittani.elan.taylor@gmail.com>
2020-11-10 10:50:30 -0500

    wonky signin HTML and pseudocode for trivia API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-const BASE_URL = 'https://opentdb.com/api.php?amount=10'
-console.log('Hello World')
-// const h1 = document.createElement('h1')
-// h1.innerHTML = 'Hello World Title'
-// document.append(h1)
+const BASE_URL = 'https://opentdb.com/api.php?amount=10&type=multiple'
+const CAT_URL = 'https://opentdb.com/api_category.php'
+//'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy'
 
 // in form create game and fetch (dynamic url based on form input)
 fetch(BASE_URL)
 .then(r => r.json())
 .then(console.log)
 
-//
+// might be part of helper for setting up category buttons
+fetch(CAT_URL)
+.then(r => r.json())
+.then(console.log)
+
+//start game helper function
+//  another container pops up with first question
+//  on click display next question and update user points
+//  after last question, display right answers(????) and 
+//      user points for game (might be a fetch here to user database to update points)
+
+// create button container for all categories/difficulty or random (same form)
+// function
+    // default buttons are random or optional selection to start game
+    // url = BASE_URL
+    // if catID
+    //      url += `&category=${catId}`
+    // if difficultyId
+    //      url += `&difficulty=${difficultyID}`
+    //  fetch(url)
+    //  .then(r.json)
+    //  .then(helper func to start)
